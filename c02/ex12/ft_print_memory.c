02e6c655bbe0580a30b64680f2b0383f89558b4f
Incheol Shin <incshin@c8r7s12.42seoul.kr>
2022-01-16 21:31:44 +0900

    c02 12 wt

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -1,6 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
 void    *ft_print_memory(void *addr, unsigned int size);
 
 void    *ft_print_memory(void *addr, unsigned int size)
 {
-    return ;
+	if (size == 0)
+		return ;
+	printf("%p", addr);
+}
+
+int	main(void)
+{
+	char	str[] = "Bonjour les aminches...c est fou.tout.ce qu on peut faire avec...print_memory....lol.lol. .";
+	int		size = sizeof(str) / sizeof(str[0]);
+
+	ft_print_memory(str, size);
+	return (0);
 }
