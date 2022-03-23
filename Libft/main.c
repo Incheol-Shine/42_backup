af2e110ec8916f483a11309d66615bcb9e4d3ac6
Incheol-Shine <parpedian@gmail.com>
2022-03-23 10:37:14 +0900

    main is main

--- Libft/main.c
+++ Libft/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include "libft.h"
 #include <stdlib.h>
@@ -29,23 +28,3 @@ int main(void)
 	printf("after: %s", str);
     return (0);
 }
-=======
-#include <stdio.h> 
-#include <stdlib.h>
-#include <string.h>
-#include "libft.h"
-
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-int 	main() 
-{ 
-	char const	str[] = "012345678901234";
-	char		*sub_str;
-
-	sub_str = ft_substr(str, 3, 6);
-	// for(int i = 0; i < 6; i++)
-	// 	printf("%c", sub_str[i]);
-	printf("%s", sub_str);
-	printf("\n");
-	free(sub_str);
-}
->>>>>>> 52eecb1a988c4b8e2cde52fbe66aa31b7ae95a6f
