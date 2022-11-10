355c737ba225eb4bfc41c2051acbb98943a129b4
Incheol Shin <incshin@c3r4s7.42seoul.kr>
2022-11-10 16:54:06 +0900

    command.c add

--- push_swap/command.c
+++ push_swap/command.c
@@ -1,6 +1,79 @@
 #include "push_swap.h"
 
-void	swap(t_stack stack)
+// void	swap(t_stack x, t_stack y)
+// {
+
+// }
+
+// void	push(t_stack x, t_stack y)
+// {
+	
+// }
+
+// void	rotate(t_stack x, t_stack y)
+// {
+	
+// }
+
+// void	reverse_rotate(t_stack x, t_stack y)
+// {
+	
+// }
+
+void	sa(t_stack a, t_stack b)
+{
+
+}
+
+void	sb(t_stack a, t_stack b)
+{
+
+}
+
+void	ss(t_stack a, t_stack b)
+{
+	sa(a, b);
+	sb(a, b);
+}
+
+void	pa(t_stack a, t_stack b)
 {
 	
+}
+
+void	pb(t_stack a, t_stack b)
+{
+	
+}
+
+void	ra(t_stack a, t_stack b)
+{
+	
+}
+
+void	rb(t_stack a, t_stack b)
+{
+	
+}
+
+void	rr(t_stack a, t_stack b)
+{
+	ra(a, b);
+	rb(a, b);
+}
+
+void	rra(t_stack a, t_stack b)
+{
+	
+}
+
+void	rrb(t_stack a, t_stack b)
+{
+	
+}
+
+void	rrr(t_stack a, t_stack b)
+{
+	rra(a, b);
+	rrb(a, b);
 }
